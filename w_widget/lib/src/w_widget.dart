e4de1b4f25779dfd27d64e3e58a9053ae9a28858
Shuxin-Lai <shuxinlai001@gmail.com>
2023-01-23 16:26:47 +0800

    feat: ok cancel dialog

--- w_widget/lib/src/w_widget.dart
+++ w_widget/lib/src/w_widget.dart
@@ -9,62 +9,73 @@ enum WConfirmDialogType {
   cancel,
   dismiss,
 }
-Future<WConfirmDialogType> showConfirmDialog(
-  BuildContext context, {
-  String? title,
-  String? confirmText,
-  String? cancelText,
-  double? width,
-  bool isDismissible = true,
-  TextStyle? titleTextStyle,
-  TextStyle? confirmTextStyle,
-  TextStyle? cancelTextStyle,
-  bool distinguishCancelAndClose = false,
-}) {
-  return WFutureUtils.createFuture<WConfirmDialogType>(
-    ({required reject, required resolve}) async {
-      if (Platform.isAndroid) {
-        await showDialog(
-            context: context,
-            builder: (context) {
-              return OkCancelContent(
-                title: title,
-                confirmTextStyle: confirmTextStyle,
-                titleTextStyle: titleTextStyle,
-                cancelTextStyle: cancelTextStyle,
-                confirmText: confirmText,
-                cancelText: cancelText,
-                width: width,
-                callback: (type) {
-                  resolve(type);
-                },
-              );
-            });
-      } else {
-        await showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            backgroundColor: Colors.transparent,
-            builder: (context) {
-              return OkCancelContent(
-                title: title,
-                confirmTextStyle: confirmTextStyle,
-                titleTextStyle: titleTextStyle,
-                cancelTextStyle: cancelTextStyle,
-                confirmText: confirmText,
-                cancelText: cancelText,
-                width: width,
-                callback: (type) {
-                  resolve(type);
-                },
-              );
-            });
-      }
-      if (distinguishCancelAndClose) {
-        resolve(WConfirmDialogType.dismiss);
-      } else {
-        resolve(WConfirmDialogType.cancel);
-      }
-    },
-  );
+
+class WWidgetHelpers {
+  static Future<WConfirmDialogType> showConfirmDialog(
+    BuildContext context, {
+    String? title,
+    String? confirmText,
+    String? cancelText,
+    double? width,
+    bool isDismissible = true,
+    TextStyle? titleTextStyle,
+    TextStyle? confirmTextStyle,
+    TextStyle? cancelTextStyle,
+    bool distinguishCancelAndClose = false,
+  }) {
+    return WFutureUtils.createFuture<WConfirmDialogType>(
+      ({required reject, required resolve}) async {
+        if (Platform.isAndroid) {
+          await showDialog(
+              context: context,
+              barrierDismissible: isDismissible,
+              builder: (context) {
+                return OkCancelContent(
+                  title: title,
+                  confirmTextStyle: confirmTextStyle,
+                  titleTextStyle: titleTextStyle,
+                  cancelTextStyle: cancelTextStyle,
+                  confirmText: confirmText,
+                  cancelText: cancelText,
+                  width: width,
+                  callback: (type) {
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    resolve(type);
+                  },
+                );
+              });
+        } else {
+          await showModalBottomSheet(
+              isDismissible: isDismissible,
+              isScrollControlled: true,
+              context: context,
+              backgroundColor: Colors.transparent,
+              builder: (context) {
+                return OkCancelContent(
+                  title: title,
+                  confirmTextStyle: confirmTextStyle,
+                  titleTextStyle: titleTextStyle,
+                  cancelTextStyle: cancelTextStyle,
+                  confirmText: confirmText,
+                  cancelText: cancelText,
+                  width: width,
+                  callback: (type) {
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    resolve(type);
+                  },
+                );
+              });
+        }
+        if (distinguishCancelAndClose) {
+          resolve(WConfirmDialogType.dismiss);
+        } else {
+          resolve(WConfirmDialogType.cancel);
+        }
+      },
+    );
+  }
 }
